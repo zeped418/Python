197d26323b7a0db1372a30db0ba5d66533bc13ac
Ignacio <carlos.zepeda.654@gmail.com>
2024-03-16 16:16:42 -0600

    diccionarios con condicionales

--- C10_Dicc_condicion.py
+++ C10_Dicc_condicion.py
@@ -16,3 +16,18 @@ texto='Hola, soy Chabela'
 unicos={letra: letra.upper() for letra in texto if letra in 'aeiou'}
 
 print(unicos)
+
+#Generar un listado de numeros pares
+
+numbers = [35, 16, 10, 34, 37, 25]
+
+even_numbers = []
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+print('v1 =>', even_numbers)
+
+# Ahora usando List Comprehension
+even_numbers_v2 = [n for n in numbers if n % 2 == 0]
+
+print('v2 =>', even_numbers_v2)
