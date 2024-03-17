462fed37212f1ca120c7827600014da010064912
Ignacio <carlos.zepeda.654@gmail.com>
2024-03-17 13:37:24 -0600

    Nota: set vs tuple vs list

---
+++ C11_lista_tupla_set.py
@@ -0,0 +1,7 @@
+"""
+            mutable  ordenado   index/slicing   duplicidad elementos
+Lista   []      Y          Y        Y               Y
+Tupla   ()      N          Y        Y               Y 
+Set     {}      Y          N        N               N
+
+"""
